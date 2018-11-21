96d1ed7886faa69b904592c06948fb4c1b1ae0b6
Mary West <westmarye1219@gmail.com>
2018-11-20 19:06:25 -0600

    friendspage

--- src/components/FriendsPage/friendsPage.js
+++ src/components/FriendsPage/friendsPage.js
@@ -50,16 +50,8 @@ const buildDropDown = (friendsArray) => {
 
 const friendsPage = () => {
   const uid = authHelpers.getCurrentUid();
-  axios.get(`${apiKeys.firebaseKeys.databaseURL}/friends.json?orderBy="uid"&equalTo="${uid}"`)
-    .then((results) => {
-      const friendsObject = results.data;
-      const friendsArray = [];
-      if (friendsObject !== null) {
-        Object.keys(friendsObject).forEach((friendId) => {
-          friendsObject[friendId].id = friendId;
-          friendsArray.push(friendsObject[friendId]);
-        });
-      }
+  friendsData.getAllFriends(uid)
+    .then((friendsArray) => {
       buildDropDown(friendsArray);
     })
     .catch((error) => {
--- src/helpers/data/friendsData.js
+++ src/helpers/data/friendsData.js
@@ -3,9 +3,24 @@ import apiKeys from '../../../db/apiKeys.json';
 
 const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
 
-const getAllFriends = (uid) => {
-  console.log('uid', uid);
-};
+const getAllFriends = uid => new Promise((resolve, reject) => {
+  axios.get(`${firebaseUrl}/friends.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((results) => {
+      const friendsObject = results.data;
+      const friendsArray = [];
+      if (friendsObject !== null) {
+        Object.keys(friendsObject).forEach((friendId) => {
+          friendsObject[friendId].id = friendId;
+          friendsArray.push(friendsObject[friendId]);
+        });
+      }
+      resolve(friendsArray);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
+
 
 const getSingleFriend = friendId => new Promise((resolve, reject) => {
   axios.get(`${firebaseUrl}/friends/${friendId}.json`)
